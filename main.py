802830d88633d48b6ca8c6800e6bcaffe9448c7f
Radhey Radhey <bibekmondal906@gmail.com>
2025-07-26 15:53:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot connected as {bot.user}")
+    print(f"Bot is ready. Logged in as {bot.user}")
     await bot.tree.sync()
 
-# Load cogs
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+# Load all cogs
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
 
-bot.run("MTM4MTMyODM2MzA1MzkxMjA3NA.Gbb0Kp.y96-QuBnYqIdMvWBz7_0VSAIGFcykYdS7_PFPs")
+bot.run(TOKEN)
